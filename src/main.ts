c86578c8f94476c1a631bb466b7a72cab4d10b0c
ADCairex <angelmartinezguardiola@gmail.com>
2024-07-16 16:38:49 +0200

    Config: First configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Athlete Wellness API')
+    .setDescription('API documentation for Athlete Wellness application')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
